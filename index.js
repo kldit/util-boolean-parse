3308bdde9ee70639218a9f81f9ec6a1439102bfa
Orlando Leite <orlleite@gmail.com>
2018-10-25 18:53:53 -0300

    Fixed function already exists

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 /*
  * https://stackoverflow.com/a/264180/539623
  */
-Boolean.parse = function (str)
+if(!Boolean.parse)
 {
-    if((/^(true|1)$/i).test(str))
+    Boolean.parse = function (str)
     {
-        return true;
-    }
-    else if((/^(false|0)$/i).test(str))
-    {
-        return false;
-    }
-    else
-    {
-        return undefined;
-    }
-};
+        if((/^(true|1)$/i).test(str))
+        {
+            return true;
+        }
+        else if((/^(false|0)$/i).test(str))
+        {
+            return false;
+        }
+        else
+        {
+            return undefined;
+        }
+    };
+}
